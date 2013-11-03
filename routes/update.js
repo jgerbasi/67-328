6411190caf8ee7f45bbabdd5a43576149013bb5a
Alex Egan <aegan@andrew.cmu.edu>
2013-11-03 00:15:59 -0400

    added routes/update.js so the server runs. Not sure what we will need and what we will need to be using though...

---
+++ routes/update.js
@@ -0,0 +1,8 @@
+
+exports.doPut = function(req, res){
+  res.end("doPut method in routes/update.js");
+};
+
+exports.doPost = function(req, res){
+  res.end("doPost method in routes/update.js");
+};
